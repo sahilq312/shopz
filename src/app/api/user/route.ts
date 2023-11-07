e5586ed5ffd2400b862762031ccbdbeefc2ded62
sahilq312 <120485501+sahilq312@users.noreply.github.com>
2023-11-08 00:30:12 +0530

    commit

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,29 +1,5 @@
-import connect from "@/lib/db";
-import User from "@/model/user";
-import { NextResponse } from "next/server";
-
-
-export async function POST(req : Request){
-    await connect();
-    const {email , password}: Partial<User> = await req.json();
-    try {
-        const newUser = new User({
-            email,
-            password
-        })
-        await newUser.save();
-        return  NextResponse.json(newUser, {status: 200})
-    } catch (error) {
-        return NextResponse.json({error}, {status:500})
-    }
-}
-export async function GET(){
-    await connect();
-    try {
-        
-        const users = await User.find()
-        return  NextResponse.json(users, {status: 200})
-    } catch (error) {
-        return NextResponse.json({error}, {status:500})
-    }
+import { NextResponse } from 'next/server'
+       
+export async function POST() {   
+   return NextResponse.json({message:'method allow'})
 }
