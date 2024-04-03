04ce46091162bf186bba8740f73914cbb8049112
sahilq312 <quraishisahil04@outlook.com>
2024-04-04 01:00:33 +0530

    fixing hosting issuse

--- components/cart/cartPage.tsx
+++ components/cart/cartPage.tsx
@@ -2,6 +2,15 @@ import { auth } from "@/auth";
 import { db } from "@/lib/db";
 import NextAuth from "next-auth";
 
+/* const cartItems = {
+  cart : [],
+  subtotal :  0,
+  total : 0,
+
+} */
+
+
+
 const Cartpage = async () => {
   const session = await auth();
 
