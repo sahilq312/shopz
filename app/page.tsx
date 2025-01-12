b260598911e4ed057ecebf22434a9818aef49db1
sahilq312 <quraishisahil04@outlook.com>
2025-01-13 05:15:08 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,10 @@ const font = Poppins({
 
 
 export default async function Home() {
+<<<<<<< Updated upstream
+=======
+  
+>>>>>>> Stashed changes
   const products = await List();
   return (
     <main className="container py-10">
